9d08760e9dcf8c5650046249fe2d277ff70ddb18
Vikykysha <sv321654@rambler.ru>
2018-02-26 22:08:44 +0300

    shen

--- shen/1113.py
+++ shen/1113.py
@@ -16,8 +16,33 @@ def gcd(a, b):
                 if i > gcd_:
                     gcd_ = i
     return gcd_
-
                 
+#Алгоритм Евклида
 
-print(gcd(6,336))
+def gcd1(a, b):
+    if a >= b:  
+        if a %  b == 0:
+            return b
+        return gcd1(b, a % b)
+    else:
+        if b %  a == 0:
+            return a
+        return gcd1(a, b % a)
+
+print(gcd1(9,24))
+
+#Другой вариант Евклида
+
+def gcd2(a, b):
+    m = a 
+    n = b
+    while m > 0 or n > 0:
+        if m > n:
+            m -= n
+        else:
+            n -= m
+    if m == 0:
+        return n
+    else:
+        return m
              
